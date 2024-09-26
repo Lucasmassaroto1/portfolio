071fe66c5c635bb9d5cc88386fb387bfddf914e6
Lucas Massaroto <102060111+Lucasmassaroto1@users.noreply.github.com>
2024-09-26 09:27:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// MENU MOBILE
 let btnMenu = document.getElementById('btn-menu');
 let menu = document.getElementById('menu-mobile');
 let overlay = document.getElementById('overlay-menu');
@@ -13,3 +14,21 @@ menu.addEventListener('click', ()=>{
 overlay.addEventListener('click', ()=>{
   menu.classList.remove('abrir-menu');
 });
+
+// ROLAGEM SUAVE
+const links = document.querySelectorAll('nav ul li a');
+
+for (const link of links) {
+  link.addEventListener('click', smoothScroll);
+}
+
+function smoothScroll(event) {
+  event.preventDefault();
+  const targetId = event.currentTarget.getAttribute("href").substring(1);
+  const targetSection = document.getElementById(targetId);
+
+  window.scrollTo({
+    top: targetSection.offsetTop,
+    behavior: "smooth"
+  });
+}
